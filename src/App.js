f36dd4beb90972a19c18cdbcc7ff744d21e22657
Fue <fher.209@gmail.com>
2020-08-13 09:12:43 -0700

    Delete all added code

--- src/App.js
+++ src/App.js
@@ -1,65 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
 
-import List from './list'
-import Trash from './trash'
-
 function App() {
-  const [history, setHistory] = useState([])
-  const [listItems, setListItems] = useState([])
-  const [trashItems, setTrashItems] = useState([])
-
-  function recordHistory() {
-    const newHistory = [...history]
-    const list = [...listItems]
-    const trash = [...trashItems]
-    const now = {list: list, trash: trash}
-    newHistory.push(now)
-    setHistory(newHistory)
-  }
-
-  function update(callback) {
-    recordHistory()
-    return callback
-  }
-
-  function undo() {
-    if (!history[0]) return
-    
-    const prevHistory = [...history]
-    const prev = prevHistory.pop()
-
-    const prevList = [...prev.list]
-    const prevTrash = [...prev.trash]
-
-    setListItems(prevList)
-    setTrashItems(prevTrash)
-    setHistory(prevHistory)
-  }
-
+ 
   return (
     <div className="App">
-      <button className='undo' onClick={undo}>
-          Undo
-      </button>
-
-      <List 
-        className='col'
-        update={update} 
-        listItems={listItems} 
-        setListItems={setListItems}
-        trashItems={trashItems}
-        setTrashItems={setTrashItems}
-      />
-
-      <Trash 
-        className = 'col'
-        update={update} 
-        listItems={listItems} 
-        setListItems={setListItems}
-        trashItems={trashItems}
-        setTrashItems={setTrashItems}
-      />
+      
     </div>
   );
 }
